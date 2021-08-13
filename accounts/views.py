eb4b93a027c440868a34b4ff9fbba26db61e14e6
madhuprakash19 <madhutp11@gmail.com>
2021-08-13 19:01:40 +0530

    Signup bug fixed

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from djang.views.generic import CreateView
+from django.views.generic import CreateView
 
 from . import forms
 # Create your views here.
 
 class SignUp(CreateView):
-    form_class = form.UserCreateForm
-    sucess_url = reverse_lazy('login')
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
--- simplesocial/urls.py
+++ simplesocial/urls.py
@@ -20,6 +20,9 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.HomePage.as_view(),name='home'),
-    path('account/',include('accounts.urls',name_space='accounts')),
-    path('accounts/',include('django.contrib.auth.urls')),
+    path('accounts/',include('accounts.urls',namespace='accounts')),
+    path('accounts/',include('django.contrib.auth.urls')),#idk wts the use of this...i think its of no use...since we defined again
+    path('test/',views.TestPage.as_view(),name='test'),
+    path('thanks/',views.ThanksPage.as_view(),name='thanks'),
+
 ]
